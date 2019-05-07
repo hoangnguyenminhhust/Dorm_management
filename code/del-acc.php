7ae82d424ce132c4822e5f5c06ec828d15587bfb
LinhHoang1110 <tranhoanglinh11101998@gmail.com>
2019-05-07 10:22:14 +0700

    final commit

--- code/del-acc.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$id=$_GET['id'];
-	$conn=mysql_connect("localhost","root","root");
-	mysql_select_db("manager",$conn);
-	$sql="delete from member where id=$id;";
-	mysql_query($sql);
-	header("location:mng-acc.php");
-?>
